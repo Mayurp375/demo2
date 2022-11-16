b1d41d3268224aaec5b99c3de9f28620266658c2
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-16 10:46:20 +0530

    uc 1 add method

--- calcutalte.java
+++ calcutalte.java
@@ -1,9 +1,11 @@
 class calcutalte{
    
-
+    public static void add(int x, int y){//uc 1
+        System.out.println(x+y);
+    }
 
     public static void main(String[] args) {
         System.out.println("welcom to uc");
-    
+        calcutalte.add(12, 013);
     }    
 }
