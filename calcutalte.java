26e37fa8d9f10155855ea4a359e28fbca73492a5
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-16 11:02:35 +0530

    added to division method to calculte.java file

--- calcutalte.java
+++ calcutalte.java
@@ -4,11 +4,17 @@ class calcutalte{
         System.out.println(x+y);
     }
 
+    public static void div(int x, int y){
+        System.out.println(x/y);
+    }
+
     public static void main(String[] args) {
         System.out.println("welcom to uc");
-        calcutalte.add(12, 013);
-        number.Sub(12, 013);
-        number.mult(12, 13);
+        calcutalte.add(12, 013);//uc 1
+        number.Sub(12, 013);//uc 2
+        number.mult(12, 13);//uc 2_1
+        calcutalte.div(12, 02);//uc 3
     }    
+    
 }
 
